0208bce620d60629ece0528e65d1a0ffdc3786eb
wasiullahkhan578 <wasiullahkhan578@gmail.com>
2026-04-30 12:35:47 +0530

    slash

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://volunteer-backend-production-2364.up.railway.app/";
+const BASE_URL = "https://volunteer-backend-production-2364.up.railway.app";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
